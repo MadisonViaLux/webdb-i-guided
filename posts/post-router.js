e205092b6922a1703e280b9fdb441e8bb493cac7
MadisonViaLux <madison.s.mckown@gmail.com>
2019-09-09 10:31:42 -0700

    9/9/2019

--- posts/post-router.js
+++ posts/post-router.js
@@ -7,78 +7,106 @@ const router = express.Router();
 
 
 
+// router.get('/', (req, res) => {
+//     db.select('id', 'title', 'contents').from('posts')
+//     // db('posts')
+//     .then(posts => {res.status(200).json(posts)})
+//     .catch(error => {
+//         res.status.apply(500).json({message: 'nothing'})
+//     })
+// });
+
 router.get('/', (req, res) => {
-    db.select('id', 'title', 'contents').from('posts')
-    // db('posts')
-    .then(posts => {res.status(200).json(posts)})
-    .catch(error => {
-        res.status.apply(500).json({message: 'nothing'})
-    })
+    db.select('*')
+        .from('posts')
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(error => {
+            res.json(error)
+        })
+
 });
 
 
+
+// router.get('/:id', (req, res) => {
+//     db('posts')
+//     .where({id: req.params.id})
+//     .first()
+//     .then(post => {res.status(200).json(post)})
+//     .catch(error => {
+//         res.status.apply(500).json({message: 'nothing'})
+//     })
+// });
 
 router.get('/:id', (req, res) => {
-    db('posts')
-    .where({id: req.params.id})
-    .first()
-    .then(post => {res.status(200).json(post)})
-    .catch(error => {
-        res.status.apply(500).json({message: 'nothing'})
-    })
+    
 });
 
 
+
+// router.post('/', (req, res) => {
+//     const post = req.body;
+//     db('posts').insert(post, 'id')
+//     .then(postID => {res.status(200).json(postID)})
+//     .catch(error => {
+//         res.status.apply(500).json({message: 'nothing'})
+//     })
+// });
 
 router.post('/', (req, res) => {
-    const post = req.body;
-    db('posts').insert(post, 'id')
-    .then(postID => {res.status(200).json(postID)})
-    .catch(error => {
-        res.status.apply(500).json({message: 'nothing'})
-    })
+    
 });
 
 
+
+// router.put('/:id', (req, res) => {
+//     const changes = req.body
+//     db('posts')
+//     .where('id', '=', req.params.id)
+//     .update(changes)
+//     .then(postID => {
+//         if(count > 0){
+//             res.status(200).json(postID)
+//         } else {
+//             res.status(400).json({message: 'nothing found'})
+//         }
+//     })
+//     .catch(error => {
+//         res.status.apply(500).json({message: 'nothing'})
+//     })
+// });
 
 router.put('/:id', (req, res) => {
-    const changes = req.body
-    db('posts')
-    .where('id', '=', req.params.id)
-    .update(changes)
-    .then(postID => {
-        if(count > 0){
-            res.status(200).json(postID)
-        } else {
-            res.status(400).json({message: 'nothing found'})
-        }
-    })
-    .catch(error => {
-        res.status.apply(500).json({message: 'nothing'})
-    })
+
 });
 
 
 
 
 
+
+
+// router.delete('/:id', (req, res) => {
+//     db('posts')
+//     .where('id', '=', req.params.id)
+//     .del()
+//     .then(postID => {
+//         if(count > 0){
+//             res.status(200).json(postID)
+//         } else {
+//             res.status(400).json({message: 'nothing found'})
+//         }
+//     })
+//     .catch(error => {
+//         res.status.apply(500).json({message: 'nothing'})
+//     })
+// });
 
 router.delete('/:id', (req, res) => {
-    db('posts')
-    .where('id', '=', req.params.id)
-    .del()
-    .then(postID => {
-        if(count > 0){
-            res.status(200).json(postID)
-        } else {
-            res.status(400).json({message: 'nothing found'})
-        }
-    })
-    .catch(error => {
-        res.status.apply(500).json({message: 'nothing'})
-    })
-});
 
+});
 
 
 
